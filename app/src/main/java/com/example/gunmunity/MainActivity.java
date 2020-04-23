748c335056d7193bbdad15f92f20c863da4d40e3
soma <ccxz84@naver.com>
2020-04-23 14:29:15 +0900

    스터디

--- app/src/main/java/com/example/gunmunity/MainActivity.java
+++ app/src/main/java/com/example/gunmunity/MainActivity.java
@@ -3,13 +3,43 @@ package com.example.gunmunity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.JsonObject;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-
+    Retrofit retrofit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        testservice service = retrofit.create(testservice.class);
 
+        Call<JsonObject> res = service.getdata();
+        Callback<JsonObject> back = new Callback<JsonObject>(){
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                System.out.println(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                System.out.println("fail");
+                t.printStackTrace();
+            }
+        };
+        res.enqueue(back);
     }
 }
--- app/src/main/java/com/example/gunmunity/testservice.java
+++ app/src/main/java/com/example/gunmunity/testservice.java
@@ -1,4 +1,15 @@
 package com.example.gunmunity;
 
+import com.google.gson.JsonObject;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface testservice {
+    @GET("todos/1")
+    public Call<JsonObject> getdata();
 }
